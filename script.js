b6029ae2d920ded7769e165b4921c2f8cffdb7d6
ovoander <trounganderwatt@outlook.com>
2023-12-05 20:56:10 -0800

    broken rn

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const addTodo = document.getElementById("add-todo");
+const input = document.getElementById("add-todo");
+const list = document.getElementById("list");
+
+function addTodo() {
+  const todoItem = document.createElement("li");
+  todoItem.textContent = input.value;
+
+  list.appendChild(todoItem);
+
+  input.value = "";
+}
+
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") addTodo();
+});
